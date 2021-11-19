3e6eb3a320061987eb08bffe49e9037be0147397
unknown <hermannzosou91@gmail.com>
2021-11-19 06:45:25 +0100

    header ok

--- pricing.php
+++ pricing.php
@@ -20,49 +20,9 @@
 </head>
 <body>
 <div id="wrapper">
-<div class="topbar">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <p class="pull-left hidden-xs"><i class="fa fa-clock-o"></i><span>Mon - Sat 8.00 - 18.00. Sunday CLOSED</span>
-        <p class="pull-right"><i class="fa fa-phone"></i>Tel No. (+001) 123-456-789</p>
-      </div>
-    </div>
-  </div>
-</div>
 	<!-- start header -->
-		<header>
-        <div class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="logo"/></a>
-                </div>
-                <div class="navbar-collapse collapse ">
-                    <ul class="nav navbar-nav">
-                        <li><a href="index.html">Home</a></li> 
-						 <li class="dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">About Us <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="about.html">Company</a></li>
-                            <li><a href="#">Our Team</a></li>
-                            <li><a href="#">News</a></li> 
-                            <li><a href="#">Investors</a></li>
-                        </ul>
-                    </li> 
-						<li><a href="services.html">Services</a></li>
-                        <li><a href="projects.html">Projects</a></li>
-                        <li class="active"><a href="pricing.html">Pricing</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-	</header><!-- end header -->
+	<?php include_once("header.html") ?>
+	<!-- end header -->
 	<section id="inner-headline">
 	<div class="container">
 	
