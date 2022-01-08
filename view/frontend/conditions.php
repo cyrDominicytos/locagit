9d8388f59b572ad5616b6e2c75b1bdd73144adf6
CaroleAdg <caroaguedegan@gmail.com>
2022-01-08 21:52:17 +0100

    modidfrontend

---
+++ view/frontend/conditions.php
@@ -0,0 +1,337 @@
+<?php $title = 'Locagite - Nous contacter'; ?>
+<?php $page = 5; ?>
+<?php ob_start(); ?>
+
+  <main id="main">
+
+
+<section style="margin-top: 150px;margin-left: 50px; ">
+
+        <h1 style="text-align: center;">CONDITIONS GÉNÉRALES D’UTILISATION</h1>
+
+        <p>Le site Internet « LocaGite » , propose de l’espace publicitaire sur Internet aux professionnels de l’immobilier désirant vendre ou louer des biens immobiliers, et des annonces aux particuliers recherchant un bien immobilier à acheter ou louer ou un terrain et une maison à faire construire.</p>
+
+        <p>L’accès au Site Internet, sa consultation et son utilisation sont subordonnés à l’acceptation sans réserve des présentes Conditions Générales d’Utilisation.</p>
+
+        <h2>1. EDITEUR DU SITE – CONTACTS - HEBERGEURS</h2>
+
+        <h3>1.1 Identification de l'Éditeur du site</h3>
+        <p>
+            <b>DIGITAL CLASSIFIEDS FRANCE (Groupe SeLoger)</b><br>
+			Société par Actions Simplifiée au capital de 715 526 149 €<br>
+			Siège social : 65, rue Ordener 75018 Paris<br>
+			RCS Paris 789 177 391<br>
+			TVA intra-communautaire : FR 297 891 77 391<br>
+			représentée par la société de droit allemand AVIV GROUP GMBH, elle-même représentée par Monsieur Ait Voncke, en qualité de gérant.
+        </p>
+
+        <p>Le Directeur de Publication du Site Internet est Monsieur Ait Voncke.</p>
+
+        <h3>1.2 Contacter la société</h3>
+
+        <p>L'Utilisateur a la possibilité de prendre contact auprès de la Société :</p>
+
+        <ul>
+            <li>- par lettre simple à l'adresse indiquée à l'article 1.1 ci-dessus ;</li>
+            <li>- par courrier électronique en utilisant le <a href="https://www.logic-immo.com/contactez-nous">formulaire
+                de contact</a> ;
+            </li>
+            <li>- par téléphone du lundi au vendredi de 8h à 19h et le samedi de 9h à 15h (appel non surtaxé) au :
+                <ul class="subList">
+                    <li>01 53 38 80 00 pour le site www.logic-immo.com</li>
+                    <li>01 53 38 44 00 pour le site neuf.logic-immo.com</li>
+                    <li>01 40 25 18 66 pour le site construire.logic-immo.com</li>
+                </ul>
+            </li>
+        </ul>
+
+        <h3>1.3 Hébergeur du Site Internet</h3>
+
+        <p>Le site Internet  est hébergé par la société Orange OCFB, 1 place des droits de l'Homme, 93210 La Plaine
+            Saint-Denis France.</p>
+
+        <p>Le numéro de téléphone de l’hébergeur est : +33 1 40 91 39 00.</p>
+
+        <h2>2. ACCÈS À L'EXTRANET CLIENT DU SITE INTERNET</h2>
+
+        <h3>2.1. Accès</h3>
+
+        <p>L’espace professionnel du Site Internet est réservé aux annonceurs clients du Site Internet. Ces derniers
+            doivent
+            saisir leurs identifiant et mot de passe dans l’espace réservé à cet effet pour accéder aux services du Site
+            Internet dans les conditions décrites à l’article 2.2 ci-dessous</p>
+
+        <p>Il appartient à l’utilisateur de l’espace professionnel de veiller à la préservation du caractère
+            confidentiel de
+            son identifiant et de son mot de passe. Il est responsable de tout accès, consultation ou utilisation de ses
+            identifiant et mot de passe et des conséquences qui pourraient en résulter pour lui ou les tiers.</p>
+
+        <h3>2.2. Les services disponibles</h3>
+
+        <p>L’extranet client permet à chaque client :</p>
+
+        <ul>
+            <li>- d’accéder aux statistiques de consultation de ses annonces mises en ligne sur le Site Internet,</li>
+            <li>- de mettre personnellement en ligne ses offres et d’y apporter directement toutes mises à jour, sous sa
+                responsabilité exclusive.
+            </li>
+        </ul>
+
+        <p>Pour obtenir une information complète sur les forfaits et les services complémentaires, ou toute autre
+            demande,
+            il est possible de contacter le service commercial à l’adresse suivante : pro@logic-immo.com pour les Sites
+            Internet « logic-immo.com », « neuf.logic-immo.com » et « construire.logic-immo.com » ou
+            serviceclient@lux-residence.com pour le site Internet « lux-residence.com ».</p>
+
+        <h2>3. PROPRIÉTÉ INTELLECTUELLE</h2>
+
+        <h3>3.1. Droits d'auteur et droits voisins</h3>
+
+        <p>L’ensemble des éléments constituant le Site Internet (textes, graphismes, logiciels, photographies, images,
+            sons,
+            plans, noms, logos, marques, créations et œuvres protégeables diverses, bases de données, etc...) ainsi que
+            le
+            Site Internet lui-même, relèvent des législations françaises et internationales sur les droits d’auteurs et
+            les
+            droits voisins du droit d’auteur, notamment les articles L. 122-4 et L. 122-5 du Code de la Propriété
+            Intellectuelle.</p>
+
+        <p>Ces éléments sont la propriété exclusive de DIGITAL CLASSIFIEDS FRANCE, hormis les éléments réalisés par des
+            intervenants extérieurs au Site Internet n’ayant pas cédé leurs droits d’auteur ou leurs droits voisins.</p>
+
+        <p>En conséquence, l’utilisateur du Site Internet s’engage notamment à ne pas :</p>
+
+        <ul>
+            <li>- utiliser ou interroger le Site Internet pour le compte ou au profit d’autrui,</li>
+            <li>- reproduire en nombre, à des fins commerciales ou non, des créations, des informations, des petites
+                annonces ou des photographies présentes sur le Site Internet,
+            </li>
+            <li>- intégrer tout ou partie du contenu du Site Internet dans un site tiers, à des fins commerciales ou
+                non,
+            </li>
+            <li>- utiliser un robot, notamment d’exploration (spider), une application de recherche ou récupération de
+                sites
+                Internet ou tout autre moyen permettant de récupérer ou d’indexer tout ou partie du contenu du Site
+                Internet, excepté en cas d’autorisation expresse et préalable de Digital Classifieds France,
+            </li>
+            <li>- copier les informations sur des supports de toute nature permettant de reconstituer tout ou partie des
+                fichiers d’origine.
+            </li>
+        </ul>
+
+        <p>Toute utilisation, reproduction, représentation ou adaptation non autorisée d'une page ou d'un élément de
+            forme
+            original du Site Internet est constitutive d'une contrefaçon engageant la responsabilité civile et pénale de
+            son
+            auteur. Elle peut aussi entraîner une violation des droits à l'image, droits des personnes ou de tous autres
+            droits et réglementations en vigueur.</p>
+
+        <p>Digital Classifieds France se réserve la possibilité d'exercer toutes voies de droit à l'encontre des
+            personnes
+            qui n'auraient pas respecté les interdictions contenues dans le présent article</p>
+
+        <h3>3.2. Droits du producteur de base de données</h3>
+
+        <p>L’utilisateur du Site Internet reconnaît irrévocablement que le Site Internet est constitué d'une ou
+            plusieurs
+            bases de données mis à disposition par Digital Classifieds France en qualité de producteur desdites bases de
+            données au sens des dispositions des articles L.341-1 et suivants du Code de la propriété
+            intellectuelle. </p>
+
+        <p>Dès lors, conformément aux dispositions de l'article L. 342-1 du même Code, l'utilisateur s'interdit de
+            procéder
+            à : </p>
+
+        <ul>
+            <li>- l'extraction par transfert permanent ou temporaire de la totalité ou d'une partie qualitativement ou
+                quantitativement substantielle du contenu d'une ou plusieurs des bases des données accessibles sur le
+                Site
+                Internet sur un autre support, par tout moyen et sous toute forme que ce soit, y compris à des fins
+                d'utilisation ou de consultation par un média et/ou un procédé(s) non autorisé(s) par la Société ;
+            </li>
+            <li>- la réutilisation, par la mise à la disposition du public de la totalité ou d'une partie
+                qualitativement ou
+                quantitativement substantielle du contenu d'une ou plusieurs des bases des données accessibles sur le
+                Site
+                Internet, quelle qu'en soit la forme, y compris par un lien hypertexte, un média et/ou un procédé(s) non
+                autorisé(s) par Digital Classifieds France;
+            </li>
+            <li>- la constitution, l'édition, la maintenance, la mise à jour, l'importation, l'exportation, la mise à
+                disposition de tiers, à titre gracieux ou onéreux, et la participation aux actes précités, d'une base de
+                données concurrente issue de tout ou partie d'une ou plusieurs des bases de données de Digital
+                Classifieds
+                France ;
+            </li>
+            <li>- la visualisation sur un écran par un autre procédé ou média que ceux par lesquels Digital Classifieds
+                France entend divulguer le Site Internet;
+            </li>
+            <li>- d'une manière générale, toute extraction, utilisation, stockage, reproduction, représentation ou
+                conservation, directe ou indirecte, partielle ou totale, y compris en mémoire tampon ou temporaire,
+                qualitativement ou quantitativement substantielle du contenu d'une ou plusieurs des bases de données de
+                Digital Classifieds France, commise par l'un des procédés visés ci-dessus est strictement prohibée, y
+                compris par un média non autorisé par Digital Classifieds France.
+            </li>
+        </ul>
+
+        <h3>3.3. Droit de marque</h3>
+
+        <p>Les marques, logos, dénominations sociales, sigles, noms commerciaux, enseignes et/ou nom de domaine de
+            Digital
+            Classifieds France (notamment « logic-immo », « logic-immo.com », « neuf.logic-immo.com », «
+            construire.logic-immo.com » et « lux-residence.com ») et/ou de ses partenaires commerciaux mentionnés sur le
+            Site Internet, constituent des signes distinctifs insusceptibles d'utilisation sans l'autorisation expresse
+            et
+            préalable de leur titulaire.</p>
+
+        <p>Toute représentation et/ou reproduction et/ou exploitation partielle ou totale de ces signes distinctifs est
+            donc
+            prohibée et constitutive de contrefaçon de marque, en application des dispositions du Livre 7 du Code de la
+            propriété intellectuelle, d'usurpation de dénomination sociale, nom commercial et de nom de domaine
+            engageant la
+            responsabilité civile délictuelle de son auteur.</p>
+
+        <h2>4. RESPONSABILITÉS</h2>
+
+        <p>Digital Classifieds France décline toute responsabilité :</p>
+
+        <ul>
+            <li>- en cas d’interruption du Site Internet pour des opérations de maintenance techniques ou
+                d’actualisation
+                des informations publiées,
+            </li>
+            <li>- en cas d’impossibilité momentanée d’accès au Site Internet (et/ou aux sites lui étant liés) en raison
+                de
+                problèmes techniques et ce quelles qu’en soient l’origine et la provenance,
+            </li>
+            <li>- en cas de dommages directs ou indirects causés à l’utilisateur, quelle qu’en soit la nature, résultant
+                du
+                contenu, de l’accès, ou de l’utilisation du Site Internet (et/ou des sites qui lui sont liés),
+            </li>
+            <li>- en cas d’utilisation anormale ou d’une exploitation illicite du Site Internet. L’utilisateur du Site
+                Internet est alors seul responsable des dommages causés aux tiers et des conséquences des réclamations
+                ou
+                actions qui pourraient en découler. L’utilisateur renonce également à exercer tout recours contre
+                Digital
+                Classifieds France dans le cas de poursuites diligentées par un tiers à son encontre du fait de
+                l’utilisation et/ou de l’exploitation illicite du site,
+            </li>
+            <li>- en cas de perte par le membre du Site Internet de son identifiant et/ou de son mot de passe ou en cas
+                d’usurpation de son identité.
+            </li>
+        </ul>
+
+        <p>Digital Classifieds France se réserve la possibilité de saisir toutes voies de droit à l’encontre de toute
+            personne qui n’aurait pas respecté les dispositions relatives aux articles 323-1 à 323-7 du code pénal, et
+            notamment qui accéderait frauduleusement, à tout ou partie d'un système de traitement automatisé de données
+            du
+            Site Internet.</p>
+
+        <h2>5. LIENS HYPERTEXTE</h2>
+
+        <h3>5.1. Liens à partir du Site Internet</h3>
+
+        <p>Le Site Internet peut contenir des liens hypertextes vers des sites exploités par des tiers. Ces liens sont
+            fournis à simple titre d’information. Digital Classifieds France n'exerce aucun contrôle sur ces sites et
+            décline toute responsabilité quant à l’accès, au contenu ou à l’utilisation de ces sites, ainsi qu’aux
+            dommages
+            pouvant résulter de la consultation des informations présentes sur ces sites. La décision d'activer ces
+            liens
+            relève de la pleine et entière responsabilité de l’internaute.</p>
+
+        <h3>5.2. Liens vers le Site Internet</h3>
+
+        <p>Aucun lien hypertexte ne peut être créé vers le Site Internet sans l’accord préalable auprès de Digital
+            Classifieds France.<br>
+            Si un internaute ou une personne morale désire créer, à partir de son site, un lien hypertexte vers le Site
+            Internet, ils doivent préalablement prendre contact avec Digital Classifieds France à l’adresse mentionnée
+            au
+            point 1. L'admissibilité ou non d’une telle demande sera transmise à l’intéressé.
+        </p>
+
+        <h2>6. CONTENU ÉDITORIAL ET AUTRES SERVICES MIS À DISPOSITION</h2>
+
+        <p>Des articles et informations pratiques relatifs au domaine immobilier sont mis à disposition des internautes
+            sur
+            le Site Internet, à titre indicatif, dans un seul but documentaire. Ils ne constituent en aucun cas une
+            alternative à la consultation d'un professionnel du droit ou du secteur concerné.</p>
+
+        <p>Digital Classifieds France attire l'attention de ses internautes sur les risques inhérents à toute opération
+            immobilière. L'utilisation des informations et services proposés sur« logic-immo.com » s’effectue sous la
+            seule
+            responsabilité de l’internaute, qui demeure seul responsable des engagements qu'il contracte.</p>
+
+        <p>Digital Classifieds France ne garantit pas la complétude, l'exactitude, la mise à jour et l'exhaustivité des
+            informations et services mis à disposition, Digital Classifieds France mettant tous les moyens disponibles
+            en
+            œuvre afin d’offrir aux internautes un contenu de qualité.</p>
+
+        <p>En conséquence, la responsabilité de Digital Classifieds France, de l’un de ses partenaires ou préposés, ne
+            pourra être recherchée au titre de l’information et des services proposés sur le site pour tout dommage, de
+            quelque nature que ce soit.</p>
+
+        <h2>7. MODIFICATIONS DES CONDITIONS GÉNÉRALES D’UTILISATION ET POLITIQUE DE CONFIDENTIALITÉ</h2>
+
+        <p>Digital Classifieds France se réserve le droit de modifier, librement et à tout moment, les Conditions
+            Générales
+            d’Utilisation et Politique de Confidentialité du Site Internet. Chaque internaute se connectant au Site
+            Internet
+            est invité à consulter régulièrement les présentes conditions d'utilisation afin de prendre connaissance de
+            changements éventuels. L’utilisation renouvelée du Site Internet au fur et à mesure de la modification de
+            ces
+            conditions d’utilisation constitue l'acceptation, par chaque utilisateur, des Conditions Générales
+            d'Utilisation
+            en vigueur.</p>
+
+        <h2>8. ATTRIBUTION DE JURIDICTION – LOI APPLICABLE</h2>
+
+        <p>Toutes contestations qui pourraient survenir au sujet de la validité, de l’interprétation, de l’acceptation
+            et de
+            l’exécution des présentes quel que soit le lieu de souscription, ou de règlement, feront l’objet d’une
+            tentative
+            de règlement amiable que les parties s’engagent à rechercher. A défaut d’y parvenir dans un délai d'un (1)
+            mois,
+            les tribunaux d’Aix-en-Provence seront seuls compétents même en cas d’appel en garantie ou de pluralité de
+            défendeurs, pour les procédures d’urgence ou conservatoires, en référé ou par requête.</p>
+
+        <p>Les présentes conditions générales sont soumises au droit français, qui détermine, au cas par cas, la loi
+            applicable. En l’absence de toute disposition impérative contraire ou en présence d’un choix dans la
+            détermination de la loi applicable, la loi française sera appliquée.</p>
+
+        <p>Le fait que Digital Classifieds France ne se prévale pas à un moment donné de l’une quelconque des
+            dispositions
+            des présentes Conditions Générales d’Utilisation ne peut être interprété comme valant renonciation à s’en
+            prévaloir ultérieurement.</p>
+
+        <h2>9. PROTECTION DES DONNÉES À CARACTÈRE PERSONNEL</h2>
+
+        <p>Conformément à la Réglementation relative à la protection des données à caractère personnel, l'internaute est
+            informé que DIGITAL CLASSIFIEDS FRANCE, en sa qualité de responsable de traitement, met en œuvre des
+            traitements
+            de données à caractère personnel dans le cadre de l’utilisation du Site Internet.</p>
+
+        <p>Pour toute information concernant le traitement des données à caractère personnel et connaître l’étendue de
+            leurs
+            droits, les internautes peuvent consulter la Politique de protection des données personnelles, disponible
+            via ce
+            lien : <a href="https://www.logic-immo.com/cgu-et-politique-de-confidentialite.html">https://www.logic-immo.com/cgu-et-politique-de-confidentialite.html</a>
+        </p>
+
+        <h2>10. COOKIES</h2>
+
+        <p>Lors de la consultation du Site Internet et/ou de l'Application, des informations sont susceptibles d'être
+            enregistrées dans des fichiers « cookies » installés dans l'ordinateur, tablette ou téléphone mobile de
+            l'internaute. Pour connaître notre politique des cookies, les internautes peuvent consulter la Politique de
+            cookies, disponible via ce lien : <a href="https://www.logic-immo.com/cgu-et-politique-de-confidentialite.html">https://www.logic-immo.com/cgu-et-politique-de-confidentialite.html</a>
+        </p>
+
+    </section>
+
+
+
+
+
+ </main><!-- End #main -->
+
+  <?php $content = ob_get_clean(); ?>
+  <!-- Loading of our website frontend template -->
+  <?php require('frontTemplate.php'); ?>
